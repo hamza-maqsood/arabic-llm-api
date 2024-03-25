1092843793a1425db4bc5c4d707fe43e69bccbe5
{ Hamza Maqsood; } <62572687+hamza-maqsood@users.noreply.github.com>
2024-03-26 01:54:43 +0400

    Update entrypoint.sh

--- nvidia/entrypoint.sh
+++ nvidia/entrypoint.sh
@@ -2,20 +2,19 @@
 
 quantize=${quantize:-$QUANTIZE}
 sharded=${sharded:-$SHARDED}
-max_input_length=${max_input_length:-$MAX_INPUT_LENGTH}
-max_total_tokens=${max_total_tokens:-$MAX_TOTAL_TOKENS}
+# max_input_length=${max_input_length:-$MAX_INPUT_LENGTH}
+# max_total_tokens=${max_total_tokens:-$MAX_TOTAL_TOKENS}
 
 
 # Use command-line arguments if provided, else use environment variables
 model_id=${model_id:-$MODEL_ID}
-revision=${revision:-$MODEL_REVISION}
+# revision=${revision:-$MODEL_REVISION}
 port=${port:-$PORT}
 hostname=${hostname:-$HOSTNAME}
 
 while getopts ":m:r:p:h:" opt; do
   case $opt in
     m) model_id="$OPTARG";;
-    r) revision="$OPTARG";;
     p) port="$OPTARG";;
     h) hostname="$OPTARG";;
     \?) echo "Invalid option: -$OPTARG" >&2; exit 1;;
@@ -33,8 +32,5 @@ text-generation-launcher \
   --model-id "$model_id" \
   --quantize "$quantize" \
   --sharded "$sharded" \
-  --max-input-length "$max_input_length" \
-  --max-total-tokens "$max_total_tokens" \
-  ${revision:+--revision "$revision"} \
   --port "$port" \
   --hostname "$hostname"
